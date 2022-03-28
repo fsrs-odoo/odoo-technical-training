15b158ccfbc8f6a7b123a2505b6f1c809e387310
fsrs-odoo <fsrs@odoo.com>
2022-03-28 22:28:37 +0000

    [REM] day3-day10 changes

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -19,22 +19,12 @@
     'category': 'Training',
     'version': '0.1',
     
-    'depends': ['sale', 'website'],
+    'depends': ['base'],
     
     'data': [
-        'security/academy_security.xml',
-        'security/ir.model.access.csv',
-        'views/academy_menuitems.xml',
-        'views/course_views.xml',
-        'views/session_views.xml',
-        'views/sales_views_inherit.xml',
-        'views/product_views_inherit.xml',
-        'wizard/sale_wizard_view.xml',
-        'reports/session_report_template.xml',
-        'views/academy_web_templates.xml',
     ],
     'demo': [
-        'demo/academy_demo.xml',
     ],
+    #Add license to remove License Warning
     'license': 'OPL-1'
 }
