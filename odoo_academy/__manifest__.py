3aca8558b9cd1ed6814527369414991f5cf19afb
fsrs-odoo <fsrs@odoo.com>
2022-03-29 06:02:33 +0000

    [Add] Day 6 Update

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -26,6 +26,7 @@
         'security/ir.model.access.csv',
         'views/academy_menuitems.xml',
         'views/course_views.xml',
+        'views/session_views.xml',
     ],
     'demo': [
         'demo/academy_demo.xml',
