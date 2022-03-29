cd33a2e2bb1c596e62438fde270dbdfd39a5f3d3
fsrs-odoo <fsrs@odoo.com>
2022-03-29 06:02:33 +0000

    [Add] Day 6 Update

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -26,6 +26,10 @@ class Course(models.Model):
 
     total_price = fields.Float(string="Total Price", readonly=True)
     
+    session_ids = fields.One2many(comodel_name='academy.session',
+                                  inverse_name='course_id', 
+                                  string='Sessions')
+    
     @api.onchange('base_price', 'additional_fee')
     def _onchange_total_price(self):
         if self.base_price < 0.00:
