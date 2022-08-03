0f03dd29d1e52443268371104685859694cca81a
fsrs-odoo <fsrs@odoo.com>
2022-08-03 20:17:13 +0000

    [ADD] Dog Module

--- dog/models/dog.py
+++ dog/models/dog.py
@@ -5,3 +5,4 @@ class Dog(models.Model):
     _name = "dog.dog"
     _description = "Many Dogs"
     breed = fields.Char(name="Breed")
+    parent_id = fields.Many2one("dog.dog", string="Parent", ondelete="cascade")
