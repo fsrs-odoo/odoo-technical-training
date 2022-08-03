80285c9050bd75a4452d1d469751a343ac29a7a6
fsrs-odoo <fsrs@odoo.com>
2022-08-03 18:29:51 +0000

    [ADD] New Module

--- dog/models/dog.py
+++ dog/models/dog.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class Dog(models.Model):
     _name = "dog.dog"
     _description = "Many Dogs"
-    name = fields.Char(name="Breed")
+    breed = fields.Char(name="Breed")
