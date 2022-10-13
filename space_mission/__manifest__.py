d092c2acdc0cee4e51e2b51e91dc68471ebea048
“fsrs-odoo” <fsrs@odoo.com>
2022-10-13 21:53:04 +0000

    [ADD] Space Mission Module, Manifest, Init File, Module Icon

---
+++ space_mission/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': '',
+    'version': '',
+    'summary': '',
+    'description": '',
+    'license": '',
+    'author': 'fsrs-odoo',
+    'website': 'www.odoo.com',
+    'category': 'Tech Training',
+    
+    'depends': ['base']
+    'data': [],
+    'demo': [],
+    
+    'assets': [],
+    
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    
+}
