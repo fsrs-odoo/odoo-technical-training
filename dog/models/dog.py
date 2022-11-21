b7703962fe4b327b6f6cd1f7d6e4164faccad5aa
“fsrs-odoo” <fsrs@odoo.com>
2022-11-21 05:51:24 +0000

    [UPD] Dog model and Demo Data

--- dog/models/dog.py
+++ dog/models/dog.py
@@ -4,5 +4,9 @@ from odoo import fields, models
 class Dog(models.Model):
     _name = "dog.dog"
     _description = "Many Dogs"
-    breed = fields.Char(name="Breed")
-    parent_id = fields.Many2one("dog.dog", string="Parent", ondelete="cascade")
+    name = fields.Char(name="Breed")
+    size = fields.Selection(selection=[('xs','Mini Toy'),
+                                       ('s','Small'),
+                                       ('m','Medium'),
+                                       ('l','Large'),
+                                       ('xl','Giant')])
